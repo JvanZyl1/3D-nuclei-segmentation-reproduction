9c1b58fa4f131c2c71ee09c317c5ce1206238c84
Ugo Coveliers-Munzi <ucoveliers@gmail.com>
2024-03-24 21:10:19 +0100

    Post Processing function

--- replication/src/post_processing.py
+++ replication/src/post_processing.py
@@ -18,13 +18,16 @@ import matplotlib.pyplot as plt
 from mpl_interactions import ipyplot as iplt
 
 
-class PostProcessing():
-    def __init__(self, image, mask):
-        # Initialise image and mask
-        self.image = image
-        self.mask = mask
+def remove_mirror_padding(image, padding=64):
+    """
+    Remove padding from the image
+    """
+    image = image.squeeze(0)
+    image = image[:, padding:-padding, padding:-padding]
+    image = image.unsqueeze(0)
+    return image
 
-    def reinterpolation(self, image, type='cubic'):
+def reinterpolation(image, type='bicubic'):
         """
         Reinterpolation restores the resolution of the image interpolated
         for segmentation and identification.
@@ -39,114 +42,6 @@ class PostProcessing():
             img_resized = scipy.ndimage.zoom(image, (scale_factor, 1, 1), order=0)
             img_resized = torch.from_numpy(img_resized).unsqueeze(0)
             return img_resized
-        
-    def remove_padding(self, path_image_augmented):
-        """
-        Remove padding from the image and mask
-        """
-        # 1) Find the bounding box of the mask
-        # Remove the extras 
-        mask_augmented = self.mask
-        path_image, image_augmented = path_image_augmented.split("/")[:-1],path_image_augmented.split("/")[-1] # image name
-        path_image = "/".join(path_image)
-        print(image_augmented)
-        #fixed_image_name
-        if 'flipped' in image_augmented:
-            image_old = image_augmented.split("_")[:2]
-            print(image_old)
-            image_old = "_".join(image_old) + ".tif"
-        else:
-            image_old = image_augmented
-        print(path_image, image_old)
-        path_fixed = os.path.join(path_image, image_old)
-        print(path_fixed)
-
-
-        path_original_image = path_fixed.replace("data_augmented", "data")
-        print(path_original_image)
-        # Find dimensions of the original image
-        image = tifffile.imread(path_original_image)
-        image_original_shape = image.shape
-
-        # Find dimensions of the augmented image
-        image_augmented = tifffile.imread(path_image_augmented)
-        image_augmented_shape = image_augmented.shape
-        # image_padding = mask_padding
-
-        # Find the padding
-        image_augmented_y = image_augmented_shape[2]
-        image_augmented_x = image_augmented_shape[3]
-        image_original_y = image_original_shape[1]
-        image_original_x = image_original_shape[2]
-
-        padding_y = (image_augmented_y - image_original_y) / 2
-        padding_x = (image_augmented_x - image_original_x) / 2
-        padding = (int(padding_y), int(padding_x))
-
-        print(image_augmented.shape, mask_augmented.shape, padding_y, padding_x)
-
-        # Crop the image and mask to the bounding box
-        image_deaugmented = image_augmented[padding[0]:-padding[0], padding[1]:-padding[1]]
-        mask_deaugmented = mask_augmented[padding[0]:-padding[0], padding[1]:-padding[1]]
-
-        return image_deaugmented, mask_deaugmented, image_old
-
-
-    def marker_based_watershed(self):
-        """
-        Marker-based watershed divides the
-        semantic segmentation region by watershed with the centre region of the
-        identified nucleus as a marker.
-        """
-        pass
-
-    def post_processing(self, image, mask, path_image):
-        self.image = image
-        self.mask = mask
-        self.image = self.reinterpolation(self.image)
-        self.mask = self.reinterpolation(self.mask, type='nearest')
-
-        self.image, self.mask, image_old = self.remove_padding(path_image)
-        return self.image, self.mask, image_old
-    
-    def folder_post_processing(self, path_image, path_mask):
-        """
-        Perform post-processing on all images in a folder
-        """
-        # Save the image in a folder : data_post_processed\ .... same as the original folder
-        new_image_folder_name = path_image.replace("data_augmented", "data_post_processed")
-        new_mask_folder_name = path_mask.replace("data_augmented", "data_post_processed")
-        os.makedirs(new_image_folder_name, exist_ok=True)
-        os.makedirs(new_mask_folder_name, exist_ok=True)
-        
-        # Loop over each .tif image and mask in the folder
-        image_files = os.listdir(path_image)
-        mask_files = os.listdir(path_mask)
-        idx = 0
-        for image_file, mask_file in zip(image_files, mask_files):
-            image_path = os.path.join(path_image, image_file)
-            mask_path = os.path.join(path_mask, mask_file)
-            self.image = tifffile.imread(image_path)
-            self.mask = tifffile.imread(mask_path)
-            image, mask, image_old = self.post_processing(self.image, self.mask, image_path)
-            # Save the image and mask in the new folder
-            new_image_file_path = os.path.join(new_image_folder_name, image_old)
-            new_mask_file_path = os.path.join(new_mask_folder_name, image_old)
-            print("image path", new_image_file_path, image)
-            print("mask path", new_mask_file_path, mask)
-            tifffile.imwrite(new_image_file_path, image)
-            tifffile.imwrite(new_mask_file_path, mask)
-            print(idx)
-            idx += 1
-
-def remove_mirror_padding(image, padding=64):
-    """
-    Remove padding from the image
-    """
-    image = image.squeeze(0)
-    image = image[:, padding:-padding, padding:-padding]
-    image = image.unsqueeze(0)
-    return image
 
 def print_image_3D(image, slice_index=0):
     if len(image.shape) == 4:
@@ -158,20 +53,51 @@ def print_image_3D(image, slice_index=0):
     control = iplt.imshow(func, slice_index=(0, n_ind-1), cmap='gray')
     plt.show()
 
+def find_folder_paths():
+    ##iterates through all subfolders in data directory and makes list of path names
+    data_image_dir = os.path.join('data', 'Images')
+    data_gt_dir = os.path.join('data', 'GroundTruth')
+    image_paths = []    
+    gt_paths = []
+    for root, dirs, files in os.walk(data_image_dir):
+        if not dirs:  #if directory has no subdirectories
+            image_paths.append(root) 
+    for root, dirs, files in os.walk(data_gt_dir):
+        if not dirs: 
+            gt_paths.append(root)
+    paths = (image_paths, gt_paths)
+    output_image_paths = [path.replace('data', 'data_output', 1) for path in image_paths]
+    output_gt_paths = [path.replace('data', 'data_output', 1) for path in gt_paths]
+    output_paths = (output_image_paths, output_gt_paths)
+    return paths, output_paths
 
-            
-if __name__ == "__main__":
-    path_image = os.path.join('data_augmented', 'Images', 'train', 'Images')
-    path_mask = os.path.join('data_augmented', 'GroundTruth', 'train', 'GroundTruth_NSN')
-    dataset = PreProcessCellDataset(path_image, path_mask)
-    items = [item for item in dataset]
-    image, mask = items[0]
-    image = remove_mirror_padding(image)
-    print(image.shape)
-    print_image_3D(image)
+def post_processing(output_folder='data_output'):
     """
-    # Initial image and mask from folders
-    image_initial = tifffile.imread(os.path.join(path_image, 'Emb01_t001.tif'))
-    mask_initial = tifffile.imread(os.path.join(path_mask, 'Emb01_t001.tif'))
-    post_processing = PostProcessing(image_initial, mask_initial)
-    post_processing.folder_post_processing(path_image, path_mask)"""
+    Iterates through all the images in the folder where all output images of QCANet are saved
+    It then undoes the interpolation and mirror padding (normal padding to make image square is left as it wont affect outputs)
+    It then saves all the processed images to a new folder called data_post_processed
+    """
+    _, augmented_paths = find_folder_paths()
+    #concatenate two lists in augmented paths
+    augmented_paths = augmented_paths[0] + augmented_paths[1] #join the ground tuth and images paths into single list
+    for folder_path in augmented_paths:
+        
+        #find amount of images in the folder
+        list_image_paths = os.listdir(folder_path)
+        list_image_paths = [path for path in list_image_paths if not 'flipped' in path]
+        list_image_paths = sorted(list_image_paths)
+        
+        
+        for image in list_image_paths:
+            image_path = os.path.join(folder_path, image)
+            ##Note: data_output is the folder were the output images of the neural net are saved (doesnt exist yet)
+            new_path = image_path.replace(output_folder, 'data_post_processed')
+            new_folder_path = folder_path.replace(output_folder, 'data_post_processed')
+            if not os.path.exists(new_folder_path):
+                os.makedirs(new_folder_path)
+            print(image_path, new_path)
+            image = tifffile.imread(image_path)
+            image = reinterpolation(image)
+            image = remove_mirror_padding(image)
+            tifffile.imwrite(new_path, image.numpy())
+            
