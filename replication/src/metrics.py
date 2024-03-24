8f8b1ce690fdea43c1afee1d9caf1ada1dcb2914
Ugo Coveliers-Munzi <ucoveliers@gmail.com>
2024-03-24 18:00:03 +0100

    Update metrics.py

--- replication/src/metrics.py
+++ replication/src/metrics.py
@@ -8,98 +8,8 @@ from typing import Callable
 from dataclasses import dataclass 
 
 
-def IoU(predictions_bin, targets_bin) -> float:
-    ''' Takes predictions and targets as *binary* tensors, 
-        and returns a single IoU score. 
-    
-        Intersection over Union loss. 
-        "Conventionally used to measure segmentation accuracy, 
-        because it comprehensively measures FP and FN rates" (p. 2) 
 
-            IoU = TP / (TP + FP + FN)
-
-        "However, because IoU is calculated for each image, it cannot
-        evaluate whether or not segmentation is accurate (i.e. nuclei 
-        are not fused)."
-    '''
-    count_pos = copy.deepcopy(predictions_bin + targets_bin)
-    count_neg = copy.deepcopy(predictions_bin - targets_bin)
-
-    TP = len(np.where(count_pos.reshape(count_pos.size)==2)[0])
-    FP = len(np.where(count_neg.reshape(count_neg.size)==1)[0])
-    FN = len(np.where(count_neg.reshape(count_neg.size)==-1)[0])
-
-    try:
-        iou = TP / float(TP + FP + FN)
-    except:
-        iou = 0
-    return iou
-
-
-def SEG(y, y_ans): 
-    ''' Structural Similarity Index (Instance Segmentation) 
-        "Represents the average of IoU of each instance by the sum
-        of the numbers of correct nuclear regions" 
-        
-            SEG = Sum_j ^(N_i) 1/(N_i) • max_i IoU (y_i, y*_j)
-        
-        Where N_i is the number of segmented nuclei, y is the seg-
-        mented nuclear region, y* is the ground truth.  ''' 
-
-    sum_iou = 0
-    true_labels = np.unique(y_ans)[1:]
-    print(f'ground truth labels {true_labels}')
-
-    for i in true_labels:
-
-        y_ans_mask = np.array((y_ans == i) * 1).astype(np.int8)
-        rp = measure.regionprops(y_ans_mask)[0]
-        bbox = rp.bbox
-
-        y_roi = y[bbox[0]:bbox[3], bbox[1]:bbox[4], bbox[2]:bbox[5]]
-        pred_labels = np.unique(y_roi)[1:]
-        best_iou, best_thr = 0, 0
-
-        for j in pred_labels:
-
-            y_mask = np.array((y == j) * 1).astype(np.int8)
-            iou, thr = IoU(y_mask, y_ans_mask)
-
-            if best_iou <= iou:
-                best_iou = iou
-                best_thr = np.max([thr, best_thr])
-
-        print(f'c{i:3} best IoU in SEG: {best_iou:.4f}')
-        if best_thr > 0.5: 
-            sum_iou += best_iou
-
-    return sum_iou / len(true_labels)
-
-
-
-def MUCov(predictions, targets):
-    ''' Mean Covariance Index (Instance Segmentation)
-        "Evaluate individual segmented nuclear regions and repre-
-        sents the average of the IoU of each instance by the sum 
-        of the numbers of segmentation regions" 
-
-            MuCov = Sum_i ^(N_j) 1/(N_j) • max_j IoU (y_i, y*j)
-        
-        Where N_j is the number of ground truth objects, y is the
-        segmented nuclear region, y* is the ground truth.  '''
-
-    sum_iou = 0
-    for i in range(1, predictions.max()+1):
-        mask_y = np.array((predictions == i) * 1).astype(np.uint8)
-        best_iou = 0
-        for j in range(1, targets.max()+1):
-            mask_y_ans = np.array((targets == j) * 1).astype(np.uint8)
-            best_iou = np.max([IoU(mask_y, mask_y_ans), best_iou])
-        print('best IoU: {}'.format(best_iou))
-        sum_iou += best_iou
-    return sum_iou / predictions.max()
-
-
+"""
 @dataclass 
 class Metrics:
     ''' Metrics relevant for this reproduction '''
@@ -124,7 +34,7 @@ class Metrics:
             'SEG': self.seg(predictions, targets),
             'MUCov': self.mucov(predictions, targets)
         }
-
+"""
 
 class DiceLoss(nn.Module):
 
@@ -140,3 +50,12 @@ class DiceLoss(nn.Module):
                 torch.sum(predictions) + torch.sum(targets) + smooth)
 
         return dice_score
+    
+def IoU(predictions, targets):
+    intersection = torch.sum(predictions * targets)
+    TP = intersection
+    FP = torch.sum(predictions) - intersection
+    FN = torch.sum(targets) - intersection
+
+    iou = TP / (TP + FP + FN)
+    return iou
